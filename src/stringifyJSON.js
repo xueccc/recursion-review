d42f4737ca9188c4423e634963777ed2060f0842
Zane Mattingly <z@matting.ly>
2018-04-24 10:37:17 -0700

    Re-doing stringifyJSON() due to lost work

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+    var result;
+
+    if (
+        typeof obj === 'number'  ||
+        typeof obj === 'boolean' ||
+        obj === null
+    ) {
+        result = ''+obj; // Quick operation that casts to String
+    } else if (typeof obj === 'string') {
+        result = wrapWithQuotes(obj);
+    } else if (Array.isArray(obj)) {
+        result = '[';
+
+        var arrayValues = [];
+        for(var i = 0; i < obj.length; i++) {
+            arrayValues.push(stringifyJSON(obj[i]));
+        }
+
+        result += arrayValues.join(',');
+        result += ']';
+    } else if (typeof obj === 'object') {
+        result = '{';
+
+        var objectResults = [];
+        for(var key in obj) {
+            if (!valueIsUnsupported(obj[key])) {
+               objectResults.push(wrapWithQuotes(key) + ':' + stringifyJSON(obj[key]));
+            }
+        }
+
+        result += objectResults.join(',');
+        result += '}';
+    }
+
+    return result;
+};
+
+var wrapWithQuotes = function(val) {
+	return '"'+val+'"';
+}
+
+var valueIsUnsupported = function(val) {
+	if (typeof val === 'function' || val === undefined) {
+		return true;
+	} else {
+		return false;
+	}
 };
